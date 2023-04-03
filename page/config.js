39049332469683b9f4d73b8ddc4f8e246bb0a69c
Ashams Chettiveettil <93456395+Ashxmas@users.noreply.github.com>
2023-04-03 19:54:55 +0530

    Delete config.js

--- page/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    endpoint: "https://faculty-availability-dashboard.documents.azure.com:443/",
-    key: "vMafL5RxxehjTBVymsnEtahAYAhnDYGEBYJAzJovmlbVIpPQnLILiCoqH9HSOV4tY4BiJP3DWv8jACDbhi0nUA==",
-    databaseId: "FacultyAvailabilityDB",
-    containerId: "FacultyAvailabilityCollection",
-    partitionKey: { kind: "Hash", paths: ["/address"] }
-};
-
-module.exports = config;
